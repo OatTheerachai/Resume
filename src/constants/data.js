434f7a3ae9b498b5342646d5a7c2490cb25f1a65
OatTheerachai <79505991+OatTheerachai@users.noreply.github.com>
2022-10-01 10:02:52 +0700

    Revert "Updates"
    This reverts commit 3e961bacc9133cc72871c859c26b7bbdd159d385.

---
+++ src/constants/data.js
@@ -0,0 +1,18 @@
+import { FaRegEnvelope, FaPhoneAlt, FaBirthdayCake } from "react-icons/fa";
+export const Contact = [
+  {
+    title: "email",
+    icon: <FaRegEnvelope />,
+    value: "Theerachai.kha@gmail.com",
+  },
+  {
+    title: "phone",
+    icon: <FaPhoneAlt />,
+    value: "091-007-4659",
+  },
+  {
+    title: "birthday",
+    icon: <FaBirthdayCake />,
+    value: "September 25,1998",
+  },
+];
---
+++ src/constants/interest.js
@@ -0,0 +1,21 @@
+export const Interests = [
+  {
+    title: "UI / UX",
+  },
+  {
+    title: "Backend developer",
+  },
+  {
+    title: "QA Automation",
+  },
+
+  {
+    title: "Web Application",
+  },
+  {
+    title: "Blockchain",
+  },
+  {
+    title: "Travel",
+  },
+];
